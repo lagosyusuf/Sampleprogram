2723f3928aa649290808fc06dfdfeff40fb17c93
root <root@ip-172-31-18-22.us-east-2.compute.internal>
2022-02-10 15:53:29 +0000

    added code to feature1 is been release

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
 class Hello{
        public static void main(String args[]){
-       System.out.println("Hello Git Version Control");       
+       System.out.println("Hello Git Version Control");
+       
+       System.out.println("Welcome to first feature branch");       
